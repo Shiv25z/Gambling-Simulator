7f9b22a69d0edf57eb6ef02540e03a2560a3c6d5
Shiv25z <shivamzagade25899@gmail.com>
2022-09-27 14:14:32 +0530

    Added UC2: Win and Loose Condition

--- com/gamblingGame/GamblingGame.java
+++ com/gamblingGame/GamblingGame.java
@@ -6,13 +6,26 @@ public class GamblingGame {
 
     public static final int stake = 100; // UC1 to start the stake with $100
     public static final int bet = 1; // and bet only $1
+    static int newStake ;
 
+    public static void winLoose(int r, int bet){
+        if (r==bet){
+            System.out.println("Gambler Wins!");
 
+        }
+        else{
+            System.out.println("Gambler Loose!");
+
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(" Welcome to Gambling Game Simulator! ");
+        Random random = new Random();
+        int r = random.nextInt(2);
+        winLoose(r,bet);
 
-        
+
+
     }
 }
