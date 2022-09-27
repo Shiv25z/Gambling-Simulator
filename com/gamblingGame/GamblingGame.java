730f72d0d13cfb58734fc1ba6fe961ff0a9e848c
Shiv25z <shivamzagade25899@gmail.com>
2022-09-27 14:50:49 +0530

    Added UC3: Gambler Resigns as he Won or Lost 50% Stake

--- com/gamblingGame/GamblingGame.java
+++ com/gamblingGame/GamblingGame.java
@@ -6,24 +6,45 @@ public class GamblingGame {
 
     public static final int stake = 100; // UC1 to start the stake with $100
     public static final int bet = 1; // and bet only $1
-    static int newStake ;
+    static int newStake = stake ;
 
-    public static void winLoose(int r, int bet){// UC2 to check randomly if the Gambler wins or loose the bet
-        if (r==bet){
-            System.out.println("Gambler Wins!");
+    public static void winLoose(int bet) {// UC2 to check randomly if the Gambler wins or loose the bet
 
+        while (newStake > 50 && newStake < 150) {// UC3 to check for calculation if 50% win or loose
+            int r = (int)Math.floor(Math.random()*10)%2;
+
+            if (r == bet) {
+                System.out.println("Gambler Wins!");
+                newStake++;
+                System.out.println("New Amount Inhand: "+newStake);
+                if (newStake == 150){
+                    System.out.println("Gambler resigns as he wins 50% more of his stake");//UC3
+                }
+                else {
+                    continue;
+                }
+
+            }
+            else {
+                System.out.println("Gambler Loose!");
+                newStake--;
+                System.out.println("New Amount Inhand: "+newStake);
+                if (newStake == 50){ // UC3
+                    System.out.println("Gambler resigns as he looses 50%  of his stake");
+                }
+                else {
+                    continue;
+                }
+
+
+                }
+
+            }
         }
-        else{
-            System.out.println("Gambler Loose!");
-
-        }
-    }
 
     public static void main(String[] args) {
         System.out.println(" Welcome to Gambling Game Simulator! ");
-        Random random = new Random();
-        int r = random.nextInt(2);
-        winLoose(r,bet);
+        winLoose(bet);
 
 
 
