a4c65ea108d5d55795949f22550aa88e990745fc
Shiv25z <shivamzagade25899@gmail.com>
2022-09-29 20:13:04 +0530

    Added UC4: Check Daily Win Loose Game for a 20 Days

--- com/gamblingGame/GamblingGame.java
+++ com/gamblingGame/GamblingGame.java
@@ -8,14 +8,19 @@ public class GamblingGame {
     public static final int bet = 1; // and bet only $1
     int newStake=stake;
 
+    static int[] winCountArr = new int[20];
+    static int[] looseCountArr = new int[20];
+    static String[] day = new String[20];
 
-    void winLoose() {// UC2 to check randomly if the Gambler wins or loose the bet
+
+
+    void winLoose(int i) {// UC2 to check randomly if the Gambler wins or loose the bet
         int winStack = 0;
         int looseStack = 0;
         int winCount = 0;
         int looseCount = 0;
         Random random= new Random();
-        
+
         while(true) {
             int gameStatus = random.nextInt(2) ;
             if (gameStatus == bet) {
@@ -27,44 +32,36 @@ public class GamblingGame {
                 looseStack += bet;
                 System.out.println("Gambler Lost the Game! Loose stack :" + looseStack);
             }
-
             if(winStack == stake / 2) {
-
-                System.out.println("Win Stack is 50% of his Stake. So, Gambler Resigning for the DAY.");
+                day[i]= "Won";
+                System.out.println("Gambler resigns for the day " + (i+1) + " as he wins 50% more of his stake");//UC3
+                System.out.println();
+                System.out.println("Day: " + (i+1) + " Won: "+winStack);// UC4 shows win of the day
+                System.out.println();
                 break;
             }
-
             if(looseStack == stake/ 2) {
-
-                System.out.println("Gambler Lost his 50% stake so gambler resigns for the day!");
+                day[i]= "Lost";
+                System.out.println("Gambler resigns for the day " + (i+1) + " as he looses 50%  of his stake");
+                System.out.println();
+                System.out.println("Day: " + (i+1) + " Lost: "+looseStack);// UC4 shows win of the day
+                System.out.println();
                 break;
             }
+            winCountArr[i]= winCount;
+            looseCountArr[i]=looseCount;
         }
     }
         void monthGame() {
-            int day=0;
-            while (day != 20) {
-                day++;
-                System.out.println("Day "+day+ " Game starts!");// Beginning of the game
-                winLoose();
-                System.out.println();
+            for(int day=1; day < 20;day++) {
+                System.out.println("Day " +(day+1)+ " Game starts!");// Beginning of the game
+                winLoose(day);
                 System.out.println("Game Ends for today!");// End of the game
-
-
-                if (newStake == 150) {
-
-                    System.out.println("Gambler resigns for the day " + day + " as he wins 50% more of his stake");//UC3
-                    System.out.println("Day: " + day + " Won: $50");// UC4 shows win of the day
-                    System.out.println();
-                } else {
-
-                    System.out.println("Gambler resigns for the day " + day + " as he looses 50%  of his stake");
-                    System.out.println("Day: " + day + " Lost: $50");// UC4 shows win of the day
-                    System.out.println();
-                }
-
+                System.out.println();
             }
-        }
+            }
+
+
 
 
 
@@ -74,6 +71,7 @@ public class GamblingGame {
         GamblingGame gb = new GamblingGame();
 
         gb.monthGame();
+
         }
 
 
